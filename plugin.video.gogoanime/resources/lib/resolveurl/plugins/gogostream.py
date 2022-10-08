1e00c633c55d81a936ef507d17a0a73d27bb47e4
Hara-Reddy <115335423+Hara-Reddy@users.noreply.github.com>
2022-10-08 14:41:20 +0530

    Update gogostream.py

--- plugin.video.gogoanime/resources/lib/resolveurl/plugins/gogostream.py
+++ plugin.video.gogoanime/resources/lib/resolveurl/plugins/gogostream.py
@@ -25,8 +25,8 @@ from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 
 class GoGoStreamResolver(ResolveGeneric):
     name = "gogostream"
-    domains = ['gogo-play.net']
-    pattern = r'(?://|\.)(gogo-play\.net)/(?:streaming|embed|load)\.php\?id=([a-zA-Z0-9]+)'
+    domains = ['gogohd.net']
+    pattern = r'(?://|\.)(gogohd.net)/(?:streaming|embed|load)\.php\?id=([a-zA-Z0-9]+)'
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/ajax.php?id={media_id}')
